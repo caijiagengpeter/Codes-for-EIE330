84161740a5e838c65fadc40020f0c889694404f7
caijiagengpeter <150642724+caijiagengpeter@users.noreply.github.com>
2024-10-26 21:14:45 +0800

    CodesForFlipflop(asynchronized)

---
+++ 2.3flip-flop/DFlipflop.v
@@ -0,0 +1,17 @@
+module DFlipflop
+(
+	input wire sys_clk,
+	input wire sys_rst,
+	input wire key,
+	
+	output reg led1
+
+);
+
+always@(posedge sys_clk or negedge sys_rst)
+	if (sys_rst == 1'b0)
+		led1 <= 1'b0;
+	else
+		led1 <= key;
+		
+endmodule
